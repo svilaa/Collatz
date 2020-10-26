b892a54ba3eabff558f7c14b14982abfc55e6b90
Sergi Vila Almenara <svilaalmenara@gmail.com>
2020-10-27 00:53:41 +0100

    Update collatz2.py

--- collatz2.py
+++ collatz2.py
@@ -1,3 +1,1 @@
-def a(n):return 1if n==1else 1+a(n*3+1)if n%2else 1+a(n/2)
-d={n:a(n)for n in range(1,100001)}
-print(max(d,key=d.get))
+def a(n):return 1if n==1else 1+a(n*3+1)if n%2else 1+a(n/2);d={a(n):n for n in range(1,100001)};print(d[max(d)])
